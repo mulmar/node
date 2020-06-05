d2ba3b139fdb0e9e820395b495e3bb1ff81a0352
Martin Mulder <martinmulder@me.com>
2020-06-05 19:54:51 +0200

    added clarify to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,7 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({apiKey: '5a3cc156bb2047a3966fbd3bf93e6616'});
+
 const handleImage = (req,res, db)=>{
 	const { id } = req.body;
 	db('users').where('id','=',req.body.id)
