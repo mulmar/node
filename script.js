50e6e62fd006113840ca2d2c42014a4bf7c49e61
Martin Mulder <martinmulder@me.com>
2020-06-06 16:04:01 +0200

    added files to gitignore

--- script.js
+++
@@ -1,8 +0,0 @@
-import largeNumber from './script2.js';
-
-const a = largeNumber;
-const b = 21;
-
-const result = a + b
-
-console.log(result);
--- script2.js
+++
@@ -1,3 +0,0 @@
-const largeNumber = 345;
-
-export default largeNumber;
