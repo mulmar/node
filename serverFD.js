0b9c6babbd740eb98c14c4d335cf690597c7c624
Martin Mulder <martinmulder@me.com>
2020-06-06 11:47:08 +0200

    cleaned up code

--- serverFD.js
+++ serverFD.js
@@ -33,9 +33,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImageCount(req, res, db)})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, bcrypt, db)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, bcrypt, db)})
-
 app.post('/imageurl', (req, res) => {image.handleImageApi(req, res)})
-
 
 app.listen(3000, ()=>{
 	console.log('Server listening on Port 3000...');
